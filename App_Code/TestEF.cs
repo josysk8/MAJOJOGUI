acdd3a7b96b05356810e424d402f99bd49fc5b5f
DESKTOP-U9Q6F0S\orion <gui.courtin@gmail.com>
2017-03-06 22:10:18 +0100

    ajout EF

---
+++ App_Code/TestEF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Description résumée de TestEF
+/// </summary>
+public class TestEF
+{
+    public TestEF()
+    {
+        maderaEntities db = new maderaEntities();
+        var query = from b in db.CLIENT select b;
+        Console.WriteLine("entries : ");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.CLIENT_ID);
+        }
+    }
+}
