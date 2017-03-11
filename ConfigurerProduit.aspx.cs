a156319b38a9194055814b78f5a9322e69e0ffc0
joseph <joseph.devarenne@viacesi.fr>
2017-03-11 17:05:43 +0100

    configurer produit

--- ConfigurerProduit.aspx.cs
+++ ConfigurerProduit.aspx.cs
@@ -10,6 +10,14 @@ public partial class ConfigurerProduit : System.Web.UI.Page
     List<Gamme> listeGamme = new List<Gamme>();
     protected void Page_Load(object sender, EventArgs e)
     {
+        for (int i =0; i < 10; i++)
+        {
+            Gamme addGamme = new Gamme();
+            addGamme.Nom = "Gamme " + i;
+            addGamme.Id = i;
+            listeGamme.Add(addGamme);
+        }
+
         if (null != Session["currentProduit"])
         {
             PRODUIT produitSelectionne = (PRODUIT)Session["currentProduit"];
@@ -23,9 +31,18 @@ public partial class ConfigurerProduit : System.Web.UI.Page
         foreach (Gamme gamme in listeGamme)
         {
             Button myButtonGamme = new Button();
-            //myButtonGamme.Text = gamme.
+            myButtonGamme.Text = gamme.Nom;
+            myButtonGamme.ID = "" + gamme.Id;
+            myButtonGamme.Click += new EventHandler(this.BtnSelectionGamme_Click);
+            myPanelGamme.Controls.Add(myButtonGamme);
         }
 
         downPanel.Controls.Add(myPanelGamme);
+    }
+
+    protected void BtnSelectionGamme_Click(object sender, EventArgs e)
+    {
+        Button BtnSelectionGamme = (Button)sender;
+        BtnSelectionGamme.Text = "cliquer";
     }
 }
