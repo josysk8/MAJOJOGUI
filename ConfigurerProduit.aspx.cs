a95ed735ea1417b94ecc482699d4cb1c9788cb71
joseph <joseph.devarenne@viacesi.fr>
2017-03-11 14:58:59 +0100

    avancer sur les interfaces"

---
+++ ConfigurerProduit.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ConfigurerProduit : System.Web.UI.Page
+{
+    List<Gamme> listeGamme = new List<Gamme>();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (null != Session["currentProduit"])
+        {
+            PRODUIT produitSelectionne = (PRODUIT)Session["currentProduit"];
+            LblNomProduit.Text = produitSelectionne.PRODUIT_NOM;
+        }
+    }
+
+    protected void BtnGamme_Click(object sender, EventArgs e)
+    {
+        Panel myPanelGamme = new Panel();
+        foreach (Gamme gamme in listeGamme)
+        {
+            Button myButtonGamme = new Button();
+            //myButtonGamme.Text = gamme.
+        }
+
+        downPanel.Controls.Add(myPanelGamme);
+    }
+}
