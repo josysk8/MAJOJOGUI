d59eee2566abbb11d25fb6695993b6a57ab686fc
jo <jonathan.guyot@sga-automation.com>
2017-03-12 16:45:26 +0100

    Comptable

--- App_Code/Dto/Comptable.cs
+++ App_Code/Dto/Comptable.cs
@@ -8,11 +8,29 @@ using System.Web;
 /// </summary>
 public class Comptable : Personnel
 {
-    protected String numeroPoste;
+    private String numeroPoste;
     public Comptable()
     {
         //
         // TODO: Add constructor logic here
         //
+    }
+
+    public Comptable(string numeroPoste, int id, String nom, String prenom, String login, String mdp, String token) : base(id, nom, prenom, login, mdp, token)
+    {
+        this.numeroPoste = numeroPoste;
+    }
+
+    public string NumeroPoste
+    {
+        get
+        {
+            return numeroPoste;
+        }
+
+        set
+        {
+            numeroPoste = value;
+        }
     }
 }
