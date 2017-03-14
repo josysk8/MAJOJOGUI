6ab364f5c6a6ce25319c44ca9a9a034478f85292
jo <jonathan.guyot@sga-automation.com>
2017-03-14 11:25:17 +0100

    maj BDD init

--- BDD_INIT.sql
+++ BDD_INIT.sql
@@ -1,6 +1,11 @@
 delete from dbo.GAMME;
 delete from dbo.TYPE_MODELE_GAMME;
 delete from dbo.MODELE_DE_GAMME;
+delete from dbo.TYPE_MODULE;
+delete from dbo.TYPE_COMPOSANT;
+delete from dbo.COMPOSANT;
+delete from dbo.FOURNISSEUR;
+
 
 insert into dbo.GAMME (GAMME_NOM, GAMME_DESCRIPTION) values ('Premium', 'Pour les plus riches.');
 insert into dbo.GAMME (GAMME_NOM, GAMME_DESCRIPTION) values ('Medium', 'Pour les plus medium.');
@@ -26,3 +31,51 @@ insert into dbo.MODELE_DE_GAMME (GAMME_ID, TYPE_MODELE_GAMME_ID, EST_PAR_DEFAUT,
 	((select GAMME_ID from GAMME where GAMME_NOM like 'Premium'), (select TYPE_MODELE_GAMME_ID from TYPE_MODELE_GAMME where TYPE_MODELE_GAMME_NOM='F3'), 0, 'Maison grande Premium 1', 'Grande maison', 3, 70),
 	((select GAMME_ID from GAMME where GAMME_NOM like 'Premium'), (select TYPE_MODELE_GAMME_ID from TYPE_MODELE_GAMME where TYPE_MODELE_GAMME_NOM='F3'), 0, 'Maison grande Premium 2', 'Grande maison', 3, 75),
 	((select GAMME_ID from GAMME where GAMME_NOM like 'Premium'), (select TYPE_MODELE_GAMME_ID from TYPE_MODELE_GAMME where TYPE_MODELE_GAMME_NOM='F4'), 0, 'Très grande maison Premium', 'Grande maison', 4, 85);
+
+
+insert into dbo.TYPE_MODULE (TYPE_MODULE_NOM, TYPE_MODULE_UNITE_USAGE,TYPE_MODULE_CARACTERISTIQUE) values 
+	('Murs extérieurs', 'm', 'Hauteur-Longueur'),
+	('Cloisons intérieures', 'm', 'Hauteur-Longueur'),
+	('Plancher sur dalle', 'm2', 'Hauteur-Longueur'),
+	('Plancher porteur', 'm2', 'Hauteur-Longueur'),
+	('Fermes de charpente', 'unité', 'Longueur'),
+	('Couverture', 'm2', 'Hauteur-Longueur');
+
+insert into dbo.TYPE_COMPOSANT (TYPE_COMPOSANT_NOM, TYPE_COMPOSANT_CARACTERISTIQUE, TYPE_COMPOSANT_UNITE_USAGE) values
+	('Lisses', 'Section en cm', 'Longueur en cm'),
+	('Eléments de montages', 'Section en cm', 'pièce'),
+	('Panneaux d’isolation et pare-pluie', 'Epaisseur en cm', 'Surface en m2'),
+	('Panneaux intermédiaires et de couverture', 'Section en mm', 'Surface en m2'),
+	('Planchers', 'Section en mm', 'Surface en m2'),
+	('Couverture (tuiles ou ardoises)', 'Longueur et largeur en mm', '');
+
+insert into dbo.FOURNISSEUR (FOURNISSEUR_SIRET, FOURNISSEUR_SIREN, FOURNISSEUR_NOM) values
+	('123456', '123456789', 'IKEA');
+
+
+insert into dbo.COMPOSANT (TYPE_COMPOSANT, FOURNISSEUR_ID, COMPOSANT_NOM, COMPOSANT_STOCK, COMPOSANT_VALEUR_CARACTERISTIQUE_1, COMPOSANT_VALEUR_CARACTERISTIQUE_2, VALEUR_AJOUTEE) values
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Lisses'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Montant vertical', 100, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Lisses'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Lisse basse', 150, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Lisses'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Lisse haute', 150, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Lisses'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Entretoise', 150, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Eléments de montages'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Sabot métallique', 1000, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Eléments de montages'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Boulon', 1200, 120, null, 0),
+	((select TYPE_COMPOSANT1 from dbo.TYPE_COMPOSANT where TYPE_COMPOSANT_NOM like 'Eléments de montages'), (select FOURNISSEUR_ID from dbo.FOURNISSEUR where FOURNISSEUR_NOM like 'IKEA'), 'Goujeon', 1500, 120, null, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
